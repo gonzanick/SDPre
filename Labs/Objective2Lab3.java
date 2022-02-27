81f5413dff0e35d2cd5c201f3464a0a9123dd9a3
Nick G <gonzanick@gmail.com>
2022-02-27 11:18:09 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 12;
+    double num2 = 22;
+    double num3 = 35;
+    Average = (num1 + num2 + num3) / 3;
+    System.out.println(Average);
+  }
+}
