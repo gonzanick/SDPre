49a9a9fda21fc1b7f113d76ef715a84b7f2fc7ba
Nick G <gonzanick@gmail.com>
2022-02-27 12:16:07 -0700

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 6;
+    double side2 = 8;
+    double hypotenuse = Math.sqrt((side1*side1)+(side2*side2));
+    System.out.println("The length of the hypotenuse with sides 6.0 and 8.0 is " + hypotenuse);
+  }
+}
