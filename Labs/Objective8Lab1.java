3690ed3acbfb9ffd08247a67548af3ad39f40764
Nick G <gonzanick@gmail.com>
2022-03-03 14:29:37 -0700

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=1; i <= 10; i++){
+      System.out.println(i);
+    }
+    for (int I=10; I >= 1; I--){
+      System.out.println(I);
+    }
+  }
+}
